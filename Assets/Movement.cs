07cfac5530649080b58a23eb5eb26ede195c44aa
Bahruz Mehrali <mehrelibehruz@gmail.com>
2024-03-15 21:01:31 +0400

    Video1: Rigidbody 2D

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-	
+    private Rigidbody2D rb;
+    [SerializeField] private float speed = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+    }
 }
